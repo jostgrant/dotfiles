2be2a575456b7db2fcc3bde40a12133dc709bb41
Jost Grant (ie: Joshua Steven Grant) <src.grant@pm.me>
2025-02-12 04:04:28 -0600

    Add run-if-hostname and run-if-username.

--- gui/stumpwm/init.lisp
+++ gui/stumpwm/init.lisp
@@ -14,3 +14,34 @@
         "keybinds.lisp"
         "specific.lisp"
         "apps.lisp")
+
+;; Day-To-Day Workstaiton.
+(run-if-hostname ("MELCHIOR" "CASPER")
+  (progn (sleep 0)))
+
+;; Main And Backup Laptop.
+(run-if-hostname ("BALTHASAR" "WILLESS")
+  (run-shell-command ""))
+
+;; Treadmill-Desk Laptop.
+(run-if-hostname ("NERVESS")
+  (run-shell-command ""))
+
+;; Repurposed Ultrabooks (Smart-Clocks).
+(run-if-hostname ("OPTIMEX" "PRIMER" "MEGATON")
+  (run-shell-command ""))
+
+;; Media-Center / HTPC.
+(run-if-hostname ("MEDIAN")
+  (run-shell-command ""))
+
+;; Bed-Room Machines.
+(run-if-hostname ("DOOM" "LOOM")
+  (run-shell-command ""))
+
+;; Home & Offsite Servers.
+(run-if-hostname ("0NSIGHT" "1NSIGHT")
+  (run-shell-command ""))
+
+(run-if-username ("MACHIN")
+  (run-shell-command ""))
--- gui/stumpwm/utils.lisp
+++ gui/stumpwm/utils.lisp
@@ -1,10 +1,16 @@
 (in-package :stumpwm)
 
+
+
+
+
 (defmacro load-config-files (config-dir &rest files)
   `(progn
      ,@(mapcar (lambda (file)
                  `(load (concat ,config-dir ,file)))
                files)))
+
+
 
 
 
@@ -23,3 +29,23 @@
         (run-shell-command "xset s off")
         (setf *caffeine-mode* t)
         (message "Caffeine mode on"))))
+
+
+
+
+
+
+(defmacro run-if-hostname (hostnames &body body)
+  "Run the body if the current hostname matches any of the given hostnames."
+  `(when (member (machine-instance) (list ,@hostnames) :test 'string=)
+     ,@body))
+
+(defmacro run-if-username (usernames &body body)
+  "Run the body if the current username matches any of the given usernames."
+  `(when (member (sb-ext:posix-getenv "USER") (list ,@usernames) :test 'string=)
+     ,@body))
+
+
+
+
+
